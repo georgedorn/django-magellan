1a8b3f947c763593b3e846f9608a83aa29c6dbee
George Dorn <georgedorn@gmail.com>
2011-05-18 14:48:33 -0700

    Initial view for search added

--- intranet/intranet_search/views.py
+++ intranet/intranet_search/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django import forms
+from django.conf import settings
+from whoosh import index, store, fields
+from django.shortcuts import render
+
+from intranet_search.models import WhooshPageIndex
+
+
+
+def search(request):
+    page_index = WhooshPageIndex()
+    query = request.GET.get('q', None)
+    if query:
+        query = query.replace('+', ' AND ').replace(' -', ' NOT ') #this is fairly lame
+        results = page_index.search(query)
+    
+    return render(request, 'search_results.html', {'query':query, 'results':results})
+
+
+
