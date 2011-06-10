798b3133295be8d716ec520d37ac5699600b3c51
George Dorn <georgedorn@gmail.com>
2011-06-10 12:17:29 -0700

    Force unicode on content

--- intranet/intranet_search/plugins/productwiki.py
+++ intranet/intranet_search/plugins/productwiki.py
@@ -1,8 +1,10 @@
 from intranet_search.models import BaseExtractor
 from django.utils.html import strip_tags
+from django.utils.encoding import force_unicode
 
 class productwiki(BaseExtractor):
     
     def get_content(self):
-        return strip_tags(self.content)
+        content = force_unicode(self.content)
+        return strip_tags(content)
 
