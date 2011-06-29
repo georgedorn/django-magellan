d585c16e67de83fa97f079a1d412344aec993fa1
George Dorn <georgedorn@gmail.com>
2011-06-29 13:32:53 -0700

    Removing httplib2 as dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
                         'Django>=1.3',
                         'Whoosh>=1.8.2',
                         'chardet>=1.0.1',
-                        'httplib2>=0.7.1',
                         'django-pagination>=1.0.7',
                         ],
  
